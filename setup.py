e8b365f938cbaf7a9e3b0f6a9058fcf11a4c420b
HarshKhatri-2000 <harshkhatri.pro@gmail.com>
2026-03-17 23:18:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="Excel Sheet_Filter Multple Sheet",
+    name="excel-filter-tool",
     version="11.0.0",
     author="HARSH KHATRI",
     author_email="harshkhatri.pro@gmail.com",
     description="Filter & Split Excel files by column values with a simple GUI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/HarshKhatri-2000/Excel-Filter-Tool_Multi-Sheet",  # ← Put your username
-    py_modules=["Excel Sheet_Filter Multple Sheet"],
+    url="https://github.com/HarshKhatri-2000/Excel-Filter-Tool_Multi-Sheet",
+    py_modules=["Excel_Sheet_Filter_Multple_Sheet"],   # ← Matches your filename!
     python_requires=">=3.7",
     install_requires=[
         "openpyxl>=3.0.0",
     ],
     entry_points={
         "console_scripts": [
-            "excel-filter-tool=excel_filter_tool:main",
-        ],
+            "excel-filter-tool=Excel_Sheet_Filter_Multple_Sheet:main",
+        ],                  #  ↑ Must match your filename too!
     },
     classifiers=[
         "Programming Language :: Python :: 3",
